8c5ffbf3d7ca8d7359e95184a4204e944efb71c9
nahid-sheikh <nahidsheikh.shariar.com>
2021-11-01 13:07:08 +0600

    user profile page siple designed

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import Head from "next/head";
 import Header from "../../components/Header/Index";
 import axios from "axios";
+import Cookies from "cookie";
+import jsCookie from "js-cookie";
+import TopSection from "./components/TopSection";
+import { useRouter } from "next/dist/client/router";
 
 export const getServerSideProps = async ({ req, res }) => {
   const { token } = req.cookies;
@@ -18,10 +22,19 @@ export const getServerSideProps = async ({ req, res }) => {
       config
     );
     const data = response.data;
+
     return {
       props: { user: data },
     };
   } catch (error) {
+    res.setHeader(
+      "Set-Cookie",
+      Cookies.serialize("token", "", {
+        httpOnly: true,
+        maxAge: 0,
+      })
+    );
+
     return {
       redirect: {
         destination: "/",
@@ -34,6 +47,18 @@ export const getServerSideProps = async ({ req, res }) => {
 const index = (props) => {
   const { user } = props.user.data;
 
+  const router = useRouter();
+
+  const logOutHandler = (text) => {
+    if (text === "logout") {
+      jsCookie.remove("token");
+      jsCookie.remove("userName");
+      jsCookie.remove("profileImg");
+      router.push("/");
+    } else if (text === "delete") {
+      alert("delete your account");
+    }
+  };
   return (
     <div>
       <Head>
@@ -47,8 +72,23 @@ const index = (props) => {
       <Header />
 
       <main className="max-w-[1366px] w-full m-auto mt-10 px-4">
-        <h1>User Profile</h1>
-        <h1>User Profile</h1>
+        <TopSection />
+        <h1 className="text-start mt-20 font-semibold text-2xl">{user.name}</h1>
+
+        <div className="mt-5 py-5 bg-gray-200 w-full px-5 flex justify-between">
+          <p
+            className="font-semibold cursor-pointer text-gray-600 mb-2"
+            onClick={() => logOutHandler("logout")}
+          >
+            LogOut
+          </p>
+          <p
+            className="font-semibold cursor-pointer text-red-600 mb-2"
+            onClick={() => logOutHandler("delete")}
+          >
+            Delete Account
+          </p>
+        </div>
       </main>
     </div>
   );
