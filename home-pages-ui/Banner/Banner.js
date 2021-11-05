6ac1cc7504fcf78f0ed7a92d13ee6f06adcb8d77
nahid-sheikh <nahidsheikh.shariar.com>
2021-11-05 19:20:55 +0600

    products data updated

--- home-pages-ui/Banner/Banner.js
+++ home-pages-ui/Banner/Banner.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Banner = () => {
-    return (
-        <div className="w-full min-h-[350px] max-h-[350px] bg-green-100">
-            <h1>banner</h1>
-        </div>
-    )
-}
+  return (
+    <div className="w-full min-h-[350px] max-h-[350px] bg-green-100 relative overflow-hidden">
+      <img
+        src="https://image.freepik.com/free-psd/online-shopping-with-laptop-mockup-template-shopping-elements_1150-38886.jpg"
+        alt="banner"
+        className="w-full min-h-[350px] object-contain"
+      />
+    </div>
+  );
+};
 
-export default Banner
+export default Banner;
