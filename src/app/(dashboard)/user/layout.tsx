334ad08292e1ff776a85cdd21795561ab2552369
ShariarSheikh <shariar.dev@gmail.com>
2023-10-11 21:27:25 +0600

    update seller page

--- src/app/(dashboard)/user/layout.tsx
+++ src/app/(dashboard)/user/layout.tsx
@@ -1,15 +1,26 @@
 'use client';
-import { useSelectedLayoutSegment } from 'next/navigation';
+import { useRouter, useSelectedLayoutSegment } from 'next/navigation';
 import { FC, ReactNode } from 'react';
 import Breadcrumb from './Breadcrumb';
 import Sidebar from './Sidebar';
+import { useAppSelector } from '@/redux/hooks';
+import { Role } from '@/app/auth/utils';
+import { LoadingPage } from '@/components/common/loading';
 
 interface IProps {
   children: ReactNode;
 }
 
 const UserLayout: FC<IProps> = ({ children }) => {
+  const { user } = useAppSelector(state => state.authSlice);
   const segment = useSelectedLayoutSegment();
+
+  const router = useRouter();
+
+  if (user?.role.includes(Role.SELLER)) {
+    router.replace(`/seller`);
+    return <LoadingPage />;
+  }
 
   return (
     <section className="w-full h-auto max-w-[1190px] px-[10px] mx-auto min-h-[60vh]">
