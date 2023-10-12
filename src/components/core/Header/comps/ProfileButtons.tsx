cdbd901d916b824c82952f08d287314b5b8436d7
ShariarSheikh <shariar.dev@gmail.com>
2023-10-12 14:41:50 +0600

    profile loading bug solve

--- src/components/core/Header/comps/ProfileButtons.tsx
+++ src/components/core/Header/comps/ProfileButtons.tsx
@@ -30,45 +30,44 @@ const ProfileButtons = () => {
     if (user?.role.includes(Role.SELLER)) return route.push(`/seller`);
   };
 
-  const profile =
-    isLoading || !user?._id ? (
-      <div className="flex w-[133px] h-[38px] justify-between items-center">
-        <Skeleton circle height={40} width={40} />
-        <Skeleton className="ml-1" height={12} width={80} />
+  const profile = isLoading ? (
+    <div className="flex w-[133px] h-[38px] justify-between items-center">
+      <Skeleton circle height={40} width={40} />
+      <Skeleton className="ml-1" height={12} width={80} />
+    </div>
+  ) : user?._id ? (
+    <button
+      onClick={() => handleRouter()}
+      className="flex items-center w-full max-w-[160px]"
+    >
+      <div className="h-[40px] w-[40px] min-w-[40px] min-h-[40px] rounded-full overflow-hidden relative">
+        {user.imgUrl ? (
+          <Image
+            fill
+            src={user.imgUrl}
+            alt="user picture"
+            className="object-cover"
+          />
+        ) : (
+          <div className="w-full h-full bg-primary text-white font-medium flex items-center justify-center rounded-full">
+            {user.firstName.charAt(0)}
+          </div>
+        )}
       </div>
-    ) : user?._id ? (
-      <button
-        onClick={() => handleRouter()}
-        className="flex items-center w-full max-w-[160px]"
-      >
-        <div className="h-[40px] w-[40px] min-w-[40px] min-h-[40px] rounded-full overflow-hidden relative">
-          {user.imgUrl ? (
-            <Image
-              fill
-              src={user.imgUrl}
-              alt="user picture"
-              className="object-cover"
-            />
-          ) : (
-            <div className="w-full h-full bg-primary text-white font-medium flex items-center justify-center rounded-full">
-              {user.firstName.charAt(0)}
-            </div>
-          )}
-        </div>
-        <p className="pl-[5px] text-[16px] text-start font-bold text-[#666666] w-full max-w-[120px] line-clamp-1">
-          {user.firstName}
+      <p className="pl-[5px] text-[16px] text-start font-bold text-[#666666] w-full max-w-[120px] line-clamp-1">
+        {user.firstName}
+      </p>
+    </button>
+  ) : (
+    <Link href="/auth">
+      <button className="flex items-center w-full max-w-[160px] min-w-[160px]">
+        <MdOutlinePersonOutline size={24} className="text-primary" />
+        <p className="ml-[6px] text-[16px] font-bold text-[#666666]">
+          Login/SignUp
         </p>
       </button>
-    ) : (
-      <Link href="/auth">
-        <button className="flex items-center w-full max-w-[160px] min-w-[160px]">
-          <MdOutlinePersonOutline size={24} className="text-primary" />
-          <p className="ml-[6px] text-[16px] font-bold text-[#666666]">
-            Login/SignUp
-          </p>
-        </button>
-      </Link>
-    );
+    </Link>
+  );
 
   return (
     <div className="flex items-center md:space-x-4 space-x-2 ml-[30px]">
